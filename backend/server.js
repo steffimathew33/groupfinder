b63acaafe6f460a1469dc337a6b52939ae8dac8d
DinhKatie <87164647+DinhKatie@users.noreply.github.com>
2024-11-11 21:26:02 -0800

    Set up testing routes for CRUD operations

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,14 @@
 const connect = require("./connect"); //Runs the file connect.js, and sets connect = connect's module.exports
 const express = require("express");
 const cors = require("cors");
+const testing = require("./testingRoutes") 
 
 const app = express();
 const PORT = 3000;
 
 app.use(cors()) //helps set up middleware
 app.use(express.json()) //parse requests in json format
+app.use(testing) //make routes accessible to other parts of the code
 
 app.listen(PORT, () => {
     connect.connectToServer() //Connects us to Mongo
