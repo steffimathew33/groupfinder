06d306ddb50536560c87016960420a736aa8bed0
DinhKatie <87164647+DinhKatie@users.noreply.github.com>
2024-11-16 18:36:38 -0800

    Testing backend routes to show up on React

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,37 @@
 // import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react'
+import axios from "axios"
 
 function App() {
+
+  const [data, setData] = useState()
+
+    function createUser() {
+      let postObject = {
+        name: "Katie Dinh",
+        email: "ikatiedinh@gmail.com",
+        text: "oh god",
+        date: new Date()
+      }
+
+      axios.post("http://localhost:3000/testing", postObject)
+    }
+  /*useEffect(() => {
+    async function getData() {
+      const response = await axios.get("http://localhost:3000/testing/6732e50ba7a10a8d1810d264")
+
+      if (response.status === 200) { //If data fetch was successful
+        setData(response.data)
+      }
+    }
+
+    getData()
+  }, [])*/ //Empty dependency array means this only runs on first launch.
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>welcome to our groupfinder lol</p>
-        {/*
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        */}
-      </header>
-    </div>
+    <>
+      <button onClick={createUser}>Create Object</button>
+    </>
   );
 }
 
