9c26e2ca36861fa602c14d0bdc2759487b6e37dd
Alyssa Ko <alyssako@g.ucla.edu>
2024-11-18 23:33:52 -0800

    added page files and basic navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,42 +1,51 @@
 // import logo from './logo.svg';
 import './App.css';
-import { useState, useEffect } from 'react'
-import { getTests, getTest, createTests, updateTests, deleteTests } from './api';
+//import { useState, useEffect } from 'react'
+//import { getTests, getTest, createTests, updateTests, deleteTests } from './api';
+//pages
+
+import {HashRouter as Router, Routes, Route} from "react-router-dom"
+import { Home } from './pages/Home'
+import { Login } from './pages/Login'
+import { Profile } from './pages/Profile'
+
+//navbar and layout 
+import { Navbar } from './components/Navbar';
+import { Layout } from './components/Layout';
 
 function App() {
 
-  const [data, setData] = useState()
+  // const [data, setData] = useState()
 
-    function updateUser() {
-      let postObject = {
-        name: "Katie Dinh",
-        email: "ikatiedinh@gmail.com",
-        text: "update",
-        date: new Date()
-      }
+  //   function updateUser() {
+  //     let postObject = {
+  //       name: "Katie Dinh",
+  //       email: "ikatiedinh@gmail.com",
+  //       text: "update",
+  //       date: new Date()
+  //     }
 
-      updateTests("6739559ba1f13899db8cd84c", postObject)
-    }
-  /*useEffect(() => {
-    async function loadAllTests() {
-      let data = await getTests()
-      if (data) {
-        setData(data)
-      }
-    }
-
-    loadAllTests()
-  }, [])*/ //Empty dependency array means this only runs on first launch
-
-
-
+  //     updateTests("6739559ba1f13899db8cd84c", postObject)
+  //   }
   
-
+  //pages
+  // Login (landing page - sign in/create profile)
+    // Home (home feed)
+    //Profile (view profile)
+    //Matches
   return (
-    <>
-      <button onClick={updateUser}>Update User</button>
-    </>
-  );
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login/>}/>
+        <Route element={<Layout/>}>
+          
+          <Route path="/home" element={<Home/>}/>
+          <Route path="/profile" element={<Profile/>}/>
+
+        </Route>
+      </Routes>
+    </Router>
+  )
 }
 
 export default App;
