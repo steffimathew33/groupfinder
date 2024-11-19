8eae72472623c801311d6f02611902381d7a0ebd
Alyssa Ko <alyssako@g.ucla.edu>
2024-11-18 23:35:07 -0800

    added file pages and basic navbar

---
+++ frontend/src/components/pageData.js
@@ -0,0 +1,13 @@
+//array of objects
+//each object represents info for one page
+
+export const pageData = [
+    {
+        name: "Home",
+        path: "/home"
+    },
+    {
+        name: "Profile",
+        path: "/profile"
+    }
+]
