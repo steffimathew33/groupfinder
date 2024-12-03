ee6cbc7d85dd559eae50eb3350f25dd965cf7b10
DinhKatie <87164647+DinhKatie@users.noreply.github.com>
2024-12-02 20:40:29 -0800

    minor change

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,14 +1,9 @@
-import { CreateGroup } from "../components/CreateGroup"
 import { SendRequestButton } from "../components/SendRequest";
 import { RequestsList } from "../components/RequestsReceived";
-import './CSS_Files/CreateGroup.css';
 
 export function Home() {
     return (
         <>
-            <div className="create-group">
-                <CreateGroup />
-            </div>
             <div>
             <h1>Test Group Request</h1>
             <SendRequestButton />
