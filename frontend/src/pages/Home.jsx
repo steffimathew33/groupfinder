6261dec5da1f2bce6aa2103e37b6006ce6aec568
DinhKatie <87164647+DinhKatie@users.noreply.github.com>
2024-12-02 20:42:22 -0800

    remove css file

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,13 @@
+import { CreateGroup } from "../components/CreateGroup"
 import { SendRequestButton } from "../components/SendRequest";
 import { RequestsList } from "../components/RequestsReceived";
 
 export function Home() {
     return (
         <>
+            <div className="create-group">
+                <CreateGroup />
+            </div>
             <div>
             <h1>Test Group Request</h1>
             <SendRequestButton />
